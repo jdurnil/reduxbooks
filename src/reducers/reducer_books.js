fd820a66e6253bfe71674c09cf4ee2ac216897d1
jdurnil <jdurnil@gmail.com>
2018-03-28 09:24:43 -0400

    first

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+    return [
+        {title: 'Javascript the good parts'},
+        {title: 'Javascript the bad'},
+        {title: 'Javascript the evil parts'},
+        {title: 'Javascript the butt parts'},
+    ]
+}
